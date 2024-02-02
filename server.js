b509a9583263cab1da4471ddb7dd65cecd173b80
renanberton <renan_berton@yahoo.com.br>
2024-02-02 15:04:32 -0300

    teste2

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const shortid = require('shortid');
-const cors = require('cors');
-const { resolve } = require('url');
-const app = express();
-const PORT = 5000;
-require('dotenv').config();
-const urlDatabase = {};
+   const express = require('express');
+   const shortid = require('shortid');
+   const cors = require('cors');
+   const { resolve } = require('url');
+   const app = express();
+   const PORT = 5000;
+   require('dotenv').config();
+   const urlDatabase = {};
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+   const corsOptions = {
+   origin: process.env.BACKEND_URL || 'https://url-shortner-brasil.netlify.app',
+   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   credentials: true,
+   optionsSuccessStatus: 204,
+   };
 
-app.use(cors(corsOptions));
-app.use(express.json());
+   app.use(cors(corsOptions));
+   app.use(express.json());
 
-app.post('/shorten', (req, res) => {
-  const { originalUrl } = req.body;
-  const shortCode = shortid.generate();
-  const shortUrl = `${process.env.BACKEND_URL}/${shortCode}`;
+   app.post('/shorten', (req, res) => {
+   const { originalUrl } = req.body;
+   const shortCode = shortid.generate();
+   const shortUrl = `${process.env.BACKEND_URL}/${shortCode}`;
 
-  urlDatabase[shortCode] = originalUrl;
+   urlDatabase[shortCode] = originalUrl;
 
-  res.json({ shortUrl });
-});
+   res.json({ shortUrl });
+   });
 
-app.get('/:shortCode', (req, res) => {
-  const { shortCode } = req.params;
-  const originalUrl = urlDatabase[shortCode];
+   app.get('/:shortCode', (req, res) => {
+   const { shortCode } = req.params;
+   const originalUrl = urlDatabase[shortCode];
 
-  if (originalUrl) {
-    res.redirect(originalUrl);
-  } else {
-    res.status(404).json({ error: 'URL not found' });
-  }
-});
+   if (originalUrl) {
+      res.redirect(originalUrl);
+   } else {
+      res.status(404).json({ error: 'URL not found' });
+   }
+   });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on '${process.env.BACKEND_URL}:${PORT}'`);
-});
+   app.listen(PORT, () => {
+   console.log(`Server is running on '${process.env.BACKEND_URL}:${PORT}'`);
+   });
