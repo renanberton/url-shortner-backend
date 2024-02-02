97e5153888ab08fef46efe0059b9d98cd995c50e
renanberton <renan_berton@yahoo.com.br>
2024-02-02 16:34:03 -0300

    testeee

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
    const urlDatabase = {};
 
    const corsOptions = {
-      origin: process.env.BACKEND_URL || 'https://url-shortner-brasil.netlify.app',
+      origin: '*',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
       optionsSuccessStatus: 204,
