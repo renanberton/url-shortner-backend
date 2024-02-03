13da61ac08eb340c5c4acb05efe8e844e28d47e8
renanberton <renan_berton@yahoo.com.br>
2024-02-03 16:00:27 -0300

    TEstando netlify

--- server.js
+++ server.js
@@ -1,44 +1,59 @@
-   const express = require('express');
-   const shortid = require('shortid');
-   const cors = require('cors');
-   const { resolve } = require('url');
-   const app = express();
-   const PORT = 5000;
-   require('dotenv').config();
-   const urlDatabase = {};
+const express = require('express');
+const shortid = require('shortid');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+require('dotenv').config();
+const urlDatabase = {};
 
-   const corsOptions = {
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-      optionsSuccessStatus: 204,
-    };
-    
-   app.use(cors(corsOptions));
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-   app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.json());
 
-   app.post('/shorten', (req, res) => {
-   const { originalUrl } = req.body;
-   const shortCode = shortid.generate();
-   const shortUrl = `https://url-shortner-brasil-back-end.netlify.app/${shortCode}`;
+app.post('/shorten', (req, res) => {
+  try {
+    const { originalUrl } = req.body;
+    const shortCode = shortid.generate();
+    const shortUrl = `${process.env.BACKEND_URL}/${shortCode}`;
 
-   urlDatabase[shortCode] = originalUrl;
+    console.log('URL original:', originalUrl);
+    console.log('Código curto gerado:', shortCode);
 
-   res.json({ shortUrl });
-   });
+    urlDatabase[shortCode] = originalUrl;
 
-   app.get('/:shortCode', (req, res) => {
-   const { shortCode } = req.params;
-   const originalUrl = urlDatabase[shortCode];
+    res.json({ shortUrl });
+  } catch (error) {
+    console.error('Erro ao encurtar URL:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao encurtar a URL. Tente novamente.' });
+  }
+});
 
-   if (originalUrl) {
-      res.redirect(originalUrl);
-   } else {
-      res.statusCode();
-   }
-   });
+app.get('/:shortCode', (req, res) => {
+  const { shortCode } = req.params;
+  const originalUrl = urlDatabase[shortCode];
 
-   app.listen(PORT, () => {
-   console.log(`Server is running on '${process.env.BACKEND_URL}:${PORT}'`);
-   });
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.status(404).json({ error: 'URL não encontrada' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em 'http://localhost:${PORT}'`);
+});
+
+
+exports.handler = async (event, context) => {
+   return {
+     statusCode: 200,
+     body: JSON.stringify({ message: 'Hello World' }),
+   };
+ };
+ 
